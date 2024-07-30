1fb5e3b6372f056a80a62f0df9a15536419981fb
Artem Popelyshev <apopelyshev@gmail.com>
2024-07-30 07:59:17 +0200

    Add Node adapter, update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
-FROM node:alpine AS builder
-WORKDIR /app
-COPY . .
-RUN npm install
-RUN npm run build
+FROM node:22-bullseye-slim
+ARG PORT
+ENV PNPM_HOME="/pnpm"
+ENV PATH="$PNPM_HOME:$PATH"
+ENV HOST="0.0.0.0"
 
-FROM nginx:alpine
-COPY --from=builder /app/dist /usr/share/nginx/html
+RUN corepack enable
+COPY . /app
+WORKDIR /app
+
+RUN pnpm install --frozen-lockfile
+RUN pnpm run build
+RUN ls -lh .
+
+EXPOSE 80
+CMD ["node", "dist/server/entry.mjs"]
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [
-		lottie(),
-		react(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-	],
+  output: "server",
+  integrations: [lottie(), react(), tailwind({
+    applyBaseStyles: false
+  })],
+  adapter: node({
+    mode: "standalone"
+  })
 });
