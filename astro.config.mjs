5324994087c0d26e548c41f3a035283d0f72cfd9
Artem Popelyshev <apopelyshev@gmail.com>
2024-07-30 07:45:59 +0200

    Update project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  })]
+	output: "server",
+	integrations: [
+		lottie(),
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 });
