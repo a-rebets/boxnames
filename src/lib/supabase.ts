e7e6c7154120757883c38c0b89fb104a3933b14b
Artem Popelyshev <apopelyshev@gmail.com>
2024-07-30 17:36:23 +0200

    env update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-	import.meta.env.SUPABASE_URL,
-	import.meta.env.SUPABASE_ANON_KEY,
+	process.env.SUPABASE_URL as string,
+	process.env.SUPABASE_ANON_KEY as string,
 );
